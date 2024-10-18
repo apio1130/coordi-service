30549ebdf76bb19283c594315c26850f5322b1f4
KiJun Kim <apio1130@naver.com>
2024-10-18 16:07:29 +0900

    Initial commit

---
+++ src/main/java/com/coordi/service/CoordiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coordi.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coordi/service/CoordiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coordi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
